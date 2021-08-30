f39257aa0e0d20677f55cf0fee299caf22f9de16
ywkuo227 <ywkuo@outlook.com>
2021-08-29 21:51:00 -0700

    Aug 29th code update #1: - Updated project works. New additions: Facility Mgmt, Tech Blog, Note Taker and E-Commerce Backend. - Updated project screenshots. - Introduced script.js and workData.js to generate project workcards depending on content of workData.

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+const workSection = $(".work");
+
+const renderWorks = () => {
+    workSection.append(`
+    <button class="workcards workcards-large" onclick="window.location.href='${workData[0].appLink}';" style="background-image: url('${workData[0].imgLink}');">
+            <div>
+            <h4>${workData[0].title}</h4>
+            <p>${workData[0].tech}</p>
+            <p class="workcards-links">
+                <a href="${workData[0].repoLink}"> GitHub Repo</a>
+            </p>
+        </div>
+    </button>
+    `);
+    
+    for (i = 1; i < workData.length; i++) {
+        workSection.append(`
+        <button class="workcards" onclick="window.location.href='${workData[i].appLink}';" style="background-image: url('${workData[i].imgLink}');">
+            <div>
+                <h4>${workData[i].title}</h4>
+                <p>${workData[i].tech}</p>
+                <p class="workcards-links">
+                    <a href="${workData[i].repoLink}">GitHub Repo</a>
+                </p>
+            </div>
+        </button>
+        `)
+    }
+
+}
+
+renderWorks();
---
+++ assets/js/workData.js
@@ -0,0 +1,52 @@
+const workData = 
+[
+    {
+        "title": "Life Happens",
+        "tech": "JavaScript / Bulma / Third-party APIs",
+        "imgLink": "./assets/images/screenshots/life_happens.png",
+        "appLink": "https://ywkuo227.github.io/life-happens/",
+        "repoLink": "https://github.com/ywkuo227/life-happens"
+    },
+    {
+        "title": "Weather Dashboard",
+        "tech": "JavaScript / Bootstrap / OpenWeather API",
+        "imgLink": "./assets/images/screenshots/weather.jpg",
+        "appLink": "https://ywkuo227.github.io/weather-dashboard/",
+        "repoLink": "https://github.com/ywkuo227/weather-dashboard"
+    },
+    {
+        "title": "Work Day Scheduler<",
+        "tech": "JavaScript / jQuery / Moment.js",
+        "imgLink": "./assets/images/screenshots/scheduler.jpeg",
+        "appLink": "https://ywkuo227.github.io/work-day-scheduler/",
+        "repoLink": "https://github.com/ywkuo227/work-day-scheduler"
+    },
+    {
+        "title": "Facility Manager",
+        "tech": "Node.js / Express.js / MySQL / MVC / Handlebars",
+        "imgLink": "./assets/images/screenshots/fac_mgmt.png",
+        "appLink": "https://dn-fac-mgmt.herokuapp.com/",
+        "repoLink": "https://github.com/DN-Vanguard/Facility_Management"
+    },
+    {
+        "title": "The Tech Blog",
+        "tech": "Node.js / Express.js / MySQL / MVC / Handlebars",
+        "imgLink": "./assets/images/screenshots/tech_blog.jpeg",
+        "appLink": "https://ywk-tech-blog.herokuapp.com/",
+        "repoLink": "https://github.com/ywkuo227/tech-blog"
+    },
+    {
+        "title": "Note Taker",
+        "tech": "Node.js / Express.js / Heroku",
+        "imgLink": "./assets/images/screenshots/note_taker.jpeg",
+        "appLink": "https://ywk-note-taker.herokuapp.com/",
+        "repoLink": "https://github.com/ywkuo227/note-taker"
+    },
+    {
+        "title": "E-Commerce Backend",
+        "tech": "Express.js / MySQL / Sequelize",
+        "imgLink": "./assets/images/screenshots/ecommerce.png",
+        "appLink": "https://drive.google.com/file/d/15QpNpx7fV19kup6EGUaaZfg9RPPz7OoC/view?usp=sharing",
+        "repoLink": "https://github.com/ywkuo227/e-commerce-backend"
+    }
+];
