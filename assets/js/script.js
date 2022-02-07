c3786c2cbd7d79eab98fbe561ed24b62383d6ecf
ywkuo227 <ywkuo@outlook.com>
2022-02-06 22:30:11 -0800

    Feb 6th code update #1: - Updated portfolio. - Added summary. - UI refinement.

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,28 +2,39 @@ const workSection = $(".work");
 
 const renderWorks = () => {
     workSection.append(`
-    <button class="workcards workcards-large" onclick="window.location.href='${workData[0].appLink}';" style="background-image: url('${workData[0].imgLink}');">
-        <div class="workcard-description">
-            <h4>${workData[0].title}</h4>
-            <p>${workData[0].tech}</p>
+    <div class="workcardwrapper-lg">
+        <button class="workcards workcards-large" onclick="window.open('${workData[0].appLink}')" style="background-image: url('${workData[0].imgLink}');">
+            <div class="workcard-description">
+                <h4>${workData[0].title}</h4>
+                <p>${workData[0].tech}</p>
+                
+            </div>
+        </button>
+        <div class="worksummarywrapper">
             <p class="workcards-links">
-                <a href="${workData[0].repoLink}"> GitHub Repo</a>
+                <a class="workcards-links" href="${workData[0].repoLink}" target="_blank"> GitHub Repo</a>
             </p>
+            <p>${workData[0].summary}</p>
         </div>
-    </button>
+    </div>
     `);
     
     for (i = 1; i < workData.length; i++) {
         workSection.append(`
-        <button class="workcards" onclick="window.location.href='${workData[i].appLink}';" style="background-image: url('${workData[i].imgLink}');">
-            <div class="workcard-description">
-                <h4>${workData[i].title}</h4>
-                <p>${workData[i].tech}</p>
+        <div class="workcardwrapper">
+            <button class="workcards" onclick="window.open('${workData[i].appLink}')" style="background-image: url('${workData[i].imgLink}');">
+                <div class="workcard-description">
+                    <h4>${workData[i].title}</h4>
+                    <p>${workData[i].tech}</p>
+                </div>
+            </button>
+            <div class="worksummarywrapper">
                 <p class="workcards-links">
-                    <a href="${workData[i].repoLink}">GitHub Repo</a>
+                    <a class="workcards-links" href="${workData[i].repoLink}" target="_blank"> GitHub Repo</a>
                 </p>
+                <p>${workData[i].summary}</p>
             </div>
-        </button>
+        </div>
         `)
     }
 }
--- assets/js/workData.js
+++ assets/js/workData.js
@@ -1,52 +1,60 @@
 const workData = 
 [
     {
-        "title": "Life Happens",
-        "tech": "JavaScript / Bulma / Third-party APIs",
-        "imgLink": "./assets/images/screenshots/life_happens.png",
-        "appLink": "https://ywkuo227.github.io/life-happens/",
-        "repoLink": "https://github.com/ywkuo227/life-happens"
+        title: "Bar Hoppers",
+        tech: "JavaScript / React.js/ MUI / Third-party APIs",
+        imgLink: "./assets/images/screenshots/bar_hoppers.jpg",
+        appLink: "https://dn-bar-hoppers.herokuapp.com/",
+        repoLink: "https://github.com/DN-Vanguard/bar-hoppers",
+        summary: "This mobile-first MERN app allows users to find drinks with confidence, explore new beverage options and keep track of their favorite drinks."
+
     },
     {
-        "title": "Weather Dashboard",
-        "tech": "JavaScript / Bootstrap / OpenWeather API",
-        "imgLink": "./assets/images/screenshots/weather.jpg",
-        "appLink": "https://ywkuo227.github.io/weather-dashboard/",
-        "repoLink": "https://github.com/ywkuo227/weather-dashboard"
+        title: "Facility Management",
+        tech: "Node.js / Express.js / MySQL / MVC / Handlebars",
+        imgLink: "./assets/images/screenshots/fac_mgmt.png",
+        appLink: "https://dn-fac-mgmt.herokuapp.com/",
+        repoLink: "https://github.com/DN-Vanguard/Facility_Management",
+        summary: "This web app has user-friendly aesthetics and allows managers to manage their employees based off: location, department and floor."
     },
     {
-        "title": "Work Day Scheduler",
-        "tech": "JavaScript / jQuery / Moment.js",
-        "imgLink": "./assets/images/screenshots/scheduler.jpeg",
-        "appLink": "https://ywkuo227.github.io/work-day-scheduler/",
-        "repoLink": "https://github.com/ywkuo227/work-day-scheduler"
+        title: "Life Happens",
+        tech: "JavaScript / Bulma / Third-party APIs",
+        imgLink: "./assets/images/screenshots/life_happens.png",
+        appLink: "https://ywkuo227.github.io/life-happens/",
+        repoLink: "https://github.com/ywkuo227/life-happens",
+        summary: "This web app allows users to have a quick glance of his/her day. The interface displays the weather of the day, add any to-do items to complete, the stock market performance as well as the local news."
     },
     {
-        "title": "Facility Management",
-        "tech": "Node.js / Express.js / MySQL / MVC / Handlebars",
-        "imgLink": "./assets/images/screenshots/fac_mgmt.png",
-        "appLink": "https://dn-fac-mgmt.herokuapp.com/",
-        "repoLink": "https://github.com/DN-Vanguard/Facility_Management"
+        title: "Weather Dashboard",
+        tech: "JavaScript / Bootstrap / OpenWeather API",
+        imgLink: "./assets/images/screenshots/weather.jpg",
+        appLink: "https://ywkuo227.github.io/weather-dashboard/",
+        repoLink: "https://github.com/ywkuo227/weather-dashboard",
+        summary: "This web app allows users to search the weather and the forecast of cities. The app also keeps track of the past six searches made by the user and keeps them stored in local storage."
     },
     {
-        "title": "The Tech Blog",
-        "tech": "Node.js / Express.js / MySQL / MVC / Handlebars",
-        "imgLink": "./assets/images/screenshots/tech_blog.jpeg",
-        "appLink": "https://ywk-tech-blog.herokuapp.com/",
-        "repoLink": "https://github.com/ywkuo227/tech-blog"
+        title: "Work Day Scheduler",
+        tech: "JavaScript / jQuery / Moment.js",
+        imgLink: "./assets/images/screenshots/scheduler.jpeg",
+        appLink: "https://ywkuo227.github.io/work-day-scheduler/",
+        repoLink: "https://github.com/ywkuo227/work-day-scheduler",
+        summary: "This a simple calendar app for scheduling user's work day. The web app displays working hours from 9AM - 5PM and color them according to the client time to indicate whether the time block is in the past, present and future."
     },
     {
-        "title": "Note Taker",
-        "tech": "Node.js / Express.js / Heroku",
-        "imgLink": "./assets/images/screenshots/note_taker.jpeg",
-        "appLink": "https://ywk-note-taker.herokuapp.com/",
-        "repoLink": "https://github.com/ywkuo227/note-taker"
+        title: "The Tech Blog",
+        tech: "Node.js / Express.js / MySQL / MVC / Handlebars",
+        imgLink: "./assets/images/screenshots/tech_blog.jpeg",
+        appLink: "https://ywk-tech-blog.herokuapp.com/",
+        repoLink: "https://github.com/ywkuo227/tech-blog",
+        summary: "This app is a CMS-sytle blog site where developers can publish their blog posts and comment on other developers' posts as well."
     },
     {
-        "title": "E-Commerce Backend",
-        "tech": "Express.js / MySQL / Sequelize",
-        "imgLink": "./assets/images/screenshots/ecommerce.png",
-        "appLink": "https://drive.google.com/file/d/15QpNpx7fV19kup6EGUaaZfg9RPPz7OoC/view?usp=sharing",
-        "repoLink": "https://github.com/ywkuo227/e-commerce-backend"
+        title: "E-Commerce Backend",
+        tech: "Express.js / MySQL / Sequelize",
+        imgLink: "./assets/images/screenshots/ecommerce.png",
+        appLink: "https://drive.google.com/file/d/15QpNpx7fV19kup6EGUaaZfg9RPPz7OoC/view?usp=sharing",
+        repoLink: "https://github.com/ywkuo227/e-commerce-backend",
+        summary: "This app serves as the foundation of a simple E-Commerce backend. Allow users to run a simple e-commerce site without start from scratch."
     }
 ];
